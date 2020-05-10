ff47379b8d2f508c883e2f4c12b74ccbd9f8686a
Prakash Jha <prakash.jha@paconsulting.com>
2020-05-10 18:11:50 +0100

    Initial files

---
+++ App.js
@@ -0,0 +1,21 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from "./src/screens/HomeScreen";
+import ComponentScreen from './src/screens/ComponentsScreen';
+import ListScreen from './src/screens/ListScreen';
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Components: ComponentScreen,
+    List: ListScreen
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "App"
+    }
+  }
+);
+
+export default createAppContainer(navigator);
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+
+const HomeScreen = () => {
+  return <View>
+      <Text style={styles.text}>Hi There!</Text>
+      <Button
+        onPress={() => console.log('Button Pressed')} 
+        title="Go to Components Demo"
+      />
+      <TouchableOpacity onPress={() => console.log('List Pressed')}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+  </View>
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default HomeScreen;
