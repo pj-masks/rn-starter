df01d1024b52203d10d5fb2e6d6fef665104ceff
Prakash Jha <prakash.jha@paconsulting.com>
2021-02-07 16:35:08 +0000

    41: Screen Boilerplate

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,21 +2,25 @@ import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
-  
+
   return <View>
-      <Text style={styles.text}>Hi There!</Text>
-      <Button
-        onPress={() => navigation.navigate('Components')} 
-        title="Go to Components Demo"
-      />
-      <Button
-        onPress={() => navigation.navigate('List')} 
-        title="Go to List Demo"
-      />
-      <Button
-        onPress={() => navigation.navigate('Image')} 
-        title="Go to Image Screen"
-      />
+    <Text style={styles.text}>Hi There!</Text>
+    <Button
+      onPress={() => navigation.navigate('Components')}
+      title="Go to Components Demo"
+    />
+    <Button
+      onPress={() => navigation.navigate('List')}
+      title="Go to List Demo"
+    />
+    <Button
+      onPress={() => navigation.navigate('Image')}
+      title="Go to Image Demo"
+    />
+    <Button
+      onPress={() => navigation.navigate('Counter')}
+      title="Go to Counter Demo"
+    />
   </View>
 };
 
