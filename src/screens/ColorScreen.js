155eb34377d03a4481314fc95014cb98577485c3
Prakash Jha <prakash.jha@paconsulting.com>
2021-07-12 13:53:21 +0100

    New commit

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, Button, FlatList } from "react-native";
 
 const ColorScreen = () => {
     const [colors, setColors] = useState([]);
@@ -10,6 +10,18 @@ const ColorScreen = () => {
             <Button title="Add a Color" onPress={() => {
                 setColors([...colors, randomRgb()])
             }} />
+
+            <FlatList
+                keyExtractor={item => item}
+                data={colors}
+                renderItem={({ item }) => {
+                    return (
+                        <View
+                            style={{ height: 100, width: 100, backgroundColor: randomRgb() }}
+                        />
+                    );
+                }}
+            />
             <View
                 style={{ height: 100, width: 100, backgroundColor: randomRgb() }}
             />
